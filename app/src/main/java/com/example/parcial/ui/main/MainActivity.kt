b1f56112997d271c112f2ded205c0fe87f109229
JoaaBaa <joaquin.b.barquin@gmail.com>
2024-05-31 21:02:52 -0300

    Nav bar lateral

--- app/src/main/java/com/example/parcial/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/parcial/ui/main/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.parcial.ui.main
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.parcial.R
 import com.example.parcial.databinding.ActivityMainBinding
 import com.example.parcial.ui.explore.ExploreFragment
@@ -12,10 +19,18 @@ import com.example.parcial.ui.profile.ProfileFragment
 import com.example.parcial.ui.search.SearchFragment
 import com.example.parcial.ui.splash.SplashFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,9 +71,24 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        drawerLayout = binding.drawerLayout
+        navigationView = binding.navView
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        setupDrawerLayout()
 
     }
-
+    private fun setupDrawerLayout(){
+        val navController = navHostFragment.navController
+        navigationView.setupWithNavController(navController)
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+    }
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.nav_host, fragment)
